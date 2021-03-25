edbe70a8fc8868611280afb55f09a615c6cad0b8
popehiflo <poolpetterhijuel@hotmail.com>
2021-03-25 10:47:01 -0500

    Variables - ejemplos

--- java-training/HolaMundo/src/HolaMundo.java
+++ java-training/HolaMundo/src/HolaMundo.java
@@ -1,7 +1,24 @@
+import java.util.Locale;
+
 public class HolaMundo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String saludar = "Hello World from Java!";
+        System.out.println(saludar);
+        System.out.println("saludar = " + saludar.toUpperCase());
+        
+        int numeric = 10;
+        System.out.println("numeric = " + numeric);
+
+        boolean valor = true;
+
+        if (valor) {
+            System.out.println("numeric = " + numeric);
+            int numeric2 = 10;
+        }
+        // variables dinamicas var
+        var numero3 = 15;
+
 
     }
 }
