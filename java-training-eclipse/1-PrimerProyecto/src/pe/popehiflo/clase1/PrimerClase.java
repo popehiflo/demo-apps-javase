f6fdff93862027395ac083b1278bec3c23df02e7
popehiflo <poolpetterhijuel@hotmail.com>
2021-04-28 22:10:39 -0500

    Se invoca metodo static dentro de static

--- java-training-eclipse/1-PrimerProyecto/src/pe/popehiflo/clase1/PrimerClase.java
+++ java-training-eclipse/1-PrimerProyecto/src/pe/popehiflo/clase1/PrimerClase.java
@@ -4,6 +4,11 @@ public class PrimerClase {
 	
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		System.out.println("Sumar: " + sumar(15, 15));
+	}
+	
+	public static int sumar(int a, int b) {
+		return a + b;
 	}
 
 }
