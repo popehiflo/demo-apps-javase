ebb33a1e8f0525b4ed445553a40c7a388e87fb3f
popehiflo <poolpetterhijuel@hotmail.com>
2021-06-23 10:47:54 -0500

    POO, Clase y atributos

---
+++ java-training-eclipse/2-POO/src/pe/popehiflo/modulo6/poo/entity/Restaurante.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package pe.popehiflo.modulo6.poo.entity;
+
+/**
+ * @author popehiflo
+ * Clase que representa entidades u objetos de restaurantes
+ */
+public class Restaurante {
+
+}
