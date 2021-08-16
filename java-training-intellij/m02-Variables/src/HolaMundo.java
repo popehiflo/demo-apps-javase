cec8f25221aa338574ea70c6e2fc5f93ceaa5015
popehiflo <poolpetterhijuel@hotmail.com>
2021-08-16 10:40:11 -0500

    Ejemplo de variables

---
+++ java-training-intellij/m02-Variables/src/HolaMundo.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Locale;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        String saludar = "Hello World from Java!";
+        System.out.println(saludar);
+        System.out.println("saludar = " + saludar.toUpperCase());
+
+        int numeric = 10;
+        System.out.println("numeric = " + numeric);
+
+        boolean valor = true;
+
+        if (valor) {
+            System.out.println("numeric = " + numeric);
+            int numeric2 = 10;
+        }
+        // variables dinámicas var
+        var numero3 = 15;
+
+        //sout - System.out.println()
+        //soutv - System.out.println("ultimaVariableQueEncuentra = " + ultimaVariableQueEncuentra)
+        System.out.println("numero3 = " + numero3);
+
+        int edadPersona = 5;
+    }
+}
