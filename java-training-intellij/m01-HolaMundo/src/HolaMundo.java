ae2257c7fbeb2eb1f858e35cf9a6862eea3a9fce
popehiflo <poolpetterhijuel@hotmail.com>
2021-08-16 10:14:19 -0500

    Hola mundo desde IntelliJ

---
+++ java-training-intellij/m01-HolaMundo/src/HolaMundo.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.Locale;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde IntelliJ");
+    }
+}
