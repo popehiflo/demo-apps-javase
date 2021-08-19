6af88b01f171dc4ed21f014ed0bb3e8faa85e4a8
popehiflo <poolpetterhijuel@hotmail.com>
2021-08-19 09:22:06 -0500

    Herencia

---
+++ java-training-eclipse/2-POO/src/pe/popehiflo/modulo6/poo/entity/Edificio.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package pe.popehiflo.modulo6.poo.entity;
+
+/**
+ * @author popehiflo
+ *
+ */
+public class Edificio {
+	/**
+	 * Identificador del restaurante
+	 */
+	private long id;
+	/**
+	 * Nombre del restaurante
+	 */
+	private String nombre;
+	/**
+	 * Slogan del restaurante
+	 */
+	private String slogan;
+	/**
+	 * Imagen del restaurante (.png)
+	 */
+	private String imagen;
+	/**
+	 * Estado Habilitado o Deshabilitado del restaurante
+	 */
+	private boolean estatus;
+	
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the slogan
+	 */
+	public String getSlogan() {
+		return slogan;
+	}
+	/**
+	 * @param slogan the slogan to set
+	 */
+	public void setSlogan(String slogan) {
+		this.slogan = slogan;
+	}
+	/**
+	 * @return the imagen
+	 */
+	public String getImagen() {
+		return imagen;
+	}
+	/**
+	 * @param imagen the imagen to set
+	 */
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	/**
+	 * @return the estatus
+	 */
+	public boolean isEstatus() {
+		return estatus;
+	}
+	/**
+	 * @param estatus the estatus to set
+	 */
+	public void setEstatus(boolean estatus) {
+		this.estatus = estatus;
+	}
+
+}
--- java-training-eclipse/2-POO/src/pe/popehiflo/modulo6/poo/entity/Restaurante.java
+++ java-training-eclipse/2-POO/src/pe/popehiflo/modulo6/poo/entity/Restaurante.java
@@ -7,27 +7,8 @@ package pe.popehiflo.modulo6.poo.entity;
  * @author popehiflo
  * Clase que representa entidades u objetos de restaurantes
  */
-public class Restaurante {
-	/**
-	 * Identificador del restaurante
-	 */
-	private long id;
-	/**
-	 * Nombre del restaurante
-	 */
-	private String nombre;
-	/**
-	 * Slogan del restaurante
-	 */
-	private String slogan;
-	/**
-	 * Imagen del restaurante (.png)
-	 */
-	private String imagen;
-	/**
-	 * Estado Habilitado o Deshabilitado del restaurante
-	 */
-	private boolean estatus;
+public class Restaurante extends Edificio {
+	
 	/**
 	 * Tipo de moneda del restaurante
 	 */
@@ -67,66 +48,6 @@ public class Restaurante {
 		this.pais = pais;
 	}
 	
-	/**
-	 * @return the id
-	 */
-	public long getId() {
-		return id;
-	}
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(long id) {
-		this.id = id;
-	}
-	/**
-	 * @return the nombre
-	 */
-	public String getNombre() {
-		return nombre;
-	}
-	/**
-	 * @param nombre the nombre to set
-	 */
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	/**
-	 * @return the slogan
-	 */
-	public String getSlogan() {
-		return slogan;
-	}
-	/**
-	 * @param slogan the slogan to set
-	 */
-	public void setSlogan(String slogan) {
-		this.slogan = slogan;
-	}
-	/**
-	 * @return the imagen
-	 */
-	public String getImagen() {
-		return imagen;
-	}
-	/**
-	 * @param imagen the imagen to set
-	 */
-	public void setImagen(String imagen) {
-		this.imagen = imagen;
-	}
-	/**
-	 * @return the estatus
-	 */
-	public boolean isEstatus() {
-		return estatus;
-	}
-	/**
-	 * @param estatus the estatus to set
-	 */
-	public void setEstatus(boolean estatus) {
-		this.estatus = estatus;
-	}
 	/**
 	 * @return the tipoMoneda
 	 */
