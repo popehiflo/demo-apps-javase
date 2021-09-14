8258aed20d6501adfa007860f01a9b8db845e855
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-14 18:25:05 -0500

    fund- hola mundo branch jdk-15-fundamentos

---
+++ java-training-15/00-Fundamentos/00-fund-HolaMundo/src/HolaMundo.java
@@ -0,0 +1,13 @@
+/*
+ * Mi clase en Java
+ */
+
+/**
+ *
+ * @author popehiflo
+ */
+public class HolaMundo {
+    public static void main(String args[]) {
+        System.out.println("Hola mundo desde JDK 15!");
+    }
+}
