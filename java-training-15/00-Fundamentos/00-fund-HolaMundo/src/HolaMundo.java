f49d8518a929c94ab08dff98881191f6abcef3d2
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-15 11:13:25 -0500

    fund- Concatenacion con Java

--- java-training-15/00-Fundamentos/00-fund-HolaMundo/src/HolaMundo.java
+++ java-training-15/00-Fundamentos/00-fund-HolaMundo/src/HolaMundo.java
@@ -7,29 +7,42 @@
  * @author popehiflo
  */
 public class HolaMundo {
+
     public static void main(String args[]) {
         System.out.println("Hola mundo desde JDK 15!");
-        
+
         //Definimos la variable
         int miVariable = 10;
         System.out.println(miVariable);
-        
+
         miVariable = 5;
         System.out.println(miVariable);
-        
+
         String miVariableCadena = "Saludos, probando token Git";
         System.out.println(miVariableCadena);
-        
+
         miVariableCadena = "Adios!";
         System.out.println(miVariableCadena);
-        
+
         //var - Inferencia de tipos en Java
         var miVar = 10;
         System.out.println("miVAr = " + miVar);
-        
+
         //Valores permitidos en el nombre de variables
         var permitido = 1;
         var _permitido = 2;
         var $permitido = 3;
+
+        //Concatenacion
+        var usuario = "Pool";
+        var titulo = "Ingenierio";
+        var union = titulo + " " + usuario;
+        System.out.println("concatenacion: " + union);
+        var i = 3;
+        var j = 4;
+        System.out.println(i + j);
+        System.out.println(i + j + usuario);
+        System.out.println(usuario + i + j);
+        System.out.println(usuario + (i + j));
     }
 }
