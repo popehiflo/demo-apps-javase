a976fd4565ce3f915d5847a3d2912a6e3641ece3
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-17 09:51:21 -0500

    fund- Tipos numericos primitivos flotantes

--- java-training-15/00-Fundamentos/00-fund-HolaMundo/src/TiposDatos.java
+++ java-training-15/00-Fundamentos/00-fund-HolaMundo/src/TiposDatos.java
@@ -36,5 +36,19 @@ public class TiposDatos {
         System.out.println("valor maximo long: "+Long.MAX_VALUE);
         long otroLong = (long)9223372036854775808F;
         System.out.println("forzado a long(9223372036854775808F): " + otroLong);
+        
+        // Tipos primitivos flotantes: float y double
+        float numeroFloat = 10.0F;
+        System.out.println("valor minimo float: "+Float.MIN_VALUE);
+        System.out.println("valor maximo float: "+Float.MAX_VALUE);
+        float otroFloat = (float)3.4028236E38D;
+        System.out.println("forzado a float(3.4028236E38): " + otroFloat);
+        
+        double numeroDouble = 10.0D;
+        System.out.println("valor minimo double: "+Double.MIN_VALUE);
+        System.out.println("valor maximo double: "+Double.MAX_VALUE);
+        double otroDouble = (double)1.7976931348623158E308;
+        System.out.println("forzado a double(3.4028236E38D): " + otroDouble);
+        
     }
 }
